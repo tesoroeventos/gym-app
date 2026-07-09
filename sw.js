a47e3a210380c6e4e486f1e37f286c4256444ee0
tesoroeventos <matiastesorodj@gmail.com>
2026-07-09 11:21:03 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'registro-v1';
+const ASSETS = [
+  '/gym-app/',
+  '/gym-app/index.html',
+  '/gym-app/manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/dexie/3.2.4/dexie.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit =>
+      hit || fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      }).catch(() => caches.match('/gym-app/index.html'))
+    )
+  );
+});
